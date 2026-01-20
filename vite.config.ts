f6a5d2319b88f64af17c1007ae548cfa5e0c4cb6
Webcraft Innovation <135718962+samranrajput@users.noreply.github.com>
2026-01-21 01:25:50 +0500

    Codeverse Studious

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Manual chunking strategy
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            // Har library ka apna chunk ban jayega (e.g., vendor-react.js)
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    // Warning limit ko thora barha sakte hain agar zarurat ho (optional)
+    chunkSizeWarningLimit: 1000,
+  },
+});
